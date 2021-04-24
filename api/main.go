0ce6ab852ea756e40365e1604eae29ede897a5eb
Juan David <JD_20rb@hotmail.com>
2021-04-24 15:44:29 +0200

    Front and back communication made possible
    Now the front-end is able to communicate with the back-end (API) through Axios.

--- api/main.go
+++ api/main.go
@@ -1,17 +1,16 @@
-package main;
+package main
 
 import (
 	//fiber http server
 	"github.com/gofiber/fiber/v2"
 	//cors
-	_ "github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 	//Json encoding
 	_ "encoding/json"
 
 	C "astara/commons"
 	M "astara/models"
-
 )
 
 func main(){
@@ -27,6 +26,10 @@ func main(){
 	app := fiber.New(fiber.Config{
 //		Prefork:				true,
 	});
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:8081",
+	}));
 
 	app.Get("/api/v1/", func(c *fiber.Ctx) error{
 		return c.JSON(t.GetAllTargets(datab));
--- astara/src/views/Home.vue
+++ astara/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Atara"/>
+    <input type="text" ref="input">
+    <button @click="clicked">Aceptar</button>
   </div>
 </template>
 
@@ -9,10 +11,22 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+const axios = require('axios');
+
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    clicked() {
+      axios.get('http://localhost:3000/api/v1/').then((response)=>{
+        console.log(response);
+      });
+      //console.log(this.$refs.input);
+      //this.$refs.input.value = "has clickado";
+
+    }
   }
 }
 </script>
