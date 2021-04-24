360bd4d726dbdbce52a8a944cef73c4a2f31b616
Juan David <JD_20rb@hotmail.com>
2021-04-24 16:13:40 +0200

    Data display implemented
    Now the front-end is able to show the elements returned by the API.

---
+++ astara/src/components/item.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="item">
+    <span>{{name}}</span>
+    <span>{{priority}}</span>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'item',
+  props:{
+    name: String,
+    priority: String
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+  .item{
+    display:flex;
+    flex-direction:row;
+  }
+</style>
--- astara/src/views/Home.vue
+++ astara/src/views/Home.vue
@@ -4,24 +4,36 @@
     <HelloWorld msg="Welcome to Atara"/>
     <input type="text" ref="input">
     <button @click="clicked">Aceptar</button>
+    <div v-for="item in items" :key="item.id">
+      <item :name="item.Name" :priority="item.Priority" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import item from '@/components/item.vue'
 
 const axios = require('axios');
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    item
+  },
+  data() {
+    return{
+      items: []
+    }
   },
   methods: {
     clicked() {
       axios.get('http://localhost:3000/api/v1/').then((response)=>{
         console.log(response);
+        this.$data.items = response.data;
+        console.log(this.$data);
       });
       //console.log(this.$refs.input);
       //this.$refs.input.value = "has clickado";
